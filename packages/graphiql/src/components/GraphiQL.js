f5cfbc56601764a45b346044a02a023af83fbe7b
riz5034 <therickyzhao@gmail.com>
2019-10-07 10:29:57 -0400

    Fix on download CSV button output

--- packages/graphiql/src/components/GraphiQL.js
+++ packages/graphiql/src/components/GraphiQL.js
@@ -855,7 +855,7 @@ export class GraphiQL extends React.Component {
             }
         }
       }
-
+      // test
       // _fetchQuery may return a subscription.
       const subscription = this._fetchQuery(
         editedQuery,
@@ -864,6 +864,9 @@ export class GraphiQL extends React.Component {
         result => {
           if (queryID === this._editorQueryID) {
             let res = result.data[Object.keys(result.data)[0]]; // Consider changing to check against dictionary
+            if (!Array.isArray(res)) res = [res]; // Needs to be array to correctly parse
+            console.log(res);
+
             // Alt code 10 (alt + 10) used to split primitive array values, match in functions parsePrimitiveArr() and findPrimCol()
             let splitter = '◙';
             jsonexport(JSON.parse(JSON.stringify(res)), {
@@ -910,8 +913,120 @@ export class GraphiQL extends React.Component {
   };
 
   handleDownloadCSV = () => {
-    console.log("Downloading CSV...");
-    this._runQueryAtCursor('csv');
+    let data = JSON.parse(this.resultComponent.props.value).data;
+    console.log("Data: ")
+    console.log(data);
+    if (Object.keys(data).length > 0) {
+      console.log("Downloading CSV...");
+      /**
+       * 
+       * Helper functions to parse primitive array
+       */
+      function toCsv(arr) {
+        let str = '';
+        for (let i in arr) {
+            str += arr[i].toString() + '\n';
+        }
+        return str;
+      }
+      
+      function findPrimCol(arr, splitter) {
+        for (let i = 0; i < arr.length; i++) {
+            if ((typeof arr[i] == 'string') && (arr[i].includes(splitter))) {
+                return i;
+            }
+        }
+        return -1;
+      }
+
+      function checkArrPrim(arr, splitter) {
+        let ind;
+        for (let i in arr) {
+            ind = findPrimCol(arr[i], splitter);
+            if (ind > -1) {
+                return ind;
+            }
+        }
+        return ind;
+      }
+
+      // Check arr for prim column
+      // If it exists, go through array and 
+      async function parsePrimitiveArr(arr, splitter) {
+        let finalArr = [];
+        let stagingArr = [];
+        let workingArr = [];
+        let primInd = findPrimCol(arr, splitter);
+        stagingArr.push(arr);
+
+        let beforePrim;
+        let prim;
+        let afterPrim;
+        
+        while (primInd > 0) {
+            for (let i in stagingArr) {
+                beforePrim = stagingArr[i].slice(0, primInd);
+                prim = stagingArr[i][primInd].split(splitter);
+                afterPrim = stagingArr[i].slice(primInd + 1);
+                for (let j in prim) {
+                    workingArr = [];
+                    workingArr = workingArr.concat(beforePrim);
+                    workingArr = workingArr.concat(prim[j]);
+                    workingArr = workingArr.concat(afterPrim);
+                    finalArr.push(workingArr);
+                }
+            }
+            primInd = checkArrPrim(finalArr, splitter);
+            stagingArr = finalArr;
+            finalArr = [];
+        }
+        return stagingArr;
+      }
+
+      async function handlePrimArr(csv, splitter) {
+        let res = csv.split('\n').map(e => e.split(',').map(e => e.trim())); 
+        let stagingArr = [];
+        let counter = 0;
+        for (let ind in res) {
+            stagingArr.push(await parsePrimitiveArr(res[ind], splitter));
+            counter ++;
+
+            if (counter == res.length) {
+                return await toCsv([].concat.apply([], stagingArr));
+            }
+        }
+      }
+
+      function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+    
+        element.style.display = 'none';
+        document.body.appendChild(element);
+    
+        element.click();
+    
+        document.body.removeChild(element);
+      }
+
+      let res = data[Object.keys(data)[0]]; // Consider changing to check against dictionary
+      console.log("Reading first object (assumed to be value to download)");
+      if (!Array.isArray(res)) res = [res]; // Needs to be array to correctly parse
+      console.log(res);
+
+      // Alt code 10 (alt + 10) used to split primitive array values, match in functions parsePrimitiveArr() and findPrimCol()
+      let splitter = '◙';
+      jsonexport(res, {
+        fillGaps: true,
+        arrayPathString: splitter
+      }, function (err, csv) {
+        // if (err) return console.log(err);
+        handlePrimArr(csv, splitter).then(output => {
+        download('output.csv', output);  
+        });
+      });
+    }
   };
 
   handlePrettifyQuery = () => {
